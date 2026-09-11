3d8b2a10e47d394a673c411fcaa5c70e63ad9c8d
Amanda Leandro Soares Do Carmo <41752743+amandaleandro@users.noreply.github.com>
2026-09-11 15:28:11 -0300

    feat(n3): register recurrence service

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { CommonModule } from "../common/common.module";
 import { EventsController } from "./events.controller";
+import { EventRecurrenceService } from "./event-recurrence.service";
 import { EventsService } from "./events.service";
 
 @Module({
   imports: [CommonModule],
   controllers: [EventsController],
-  providers: [EventsService],
-  exports: [EventsService],
+  providers: [EventsService, EventRecurrenceService],
+  exports: [EventsService, EventRecurrenceService],
 })
 export class EventsModule {}
