f3a5881df79c58b219f8eb54f7ef62fbf6a2ac20
Amanda Leandro Soares Do Carmo <41752743+amandaleandro@users.noreply.github.com>
2026-09-11 14:54:54 -0300

    feat(n3): register event duplication service

--- apps/api/src/events/events.module.ts
+++ apps/api/src/events/events.module.ts
@@ -1,12 +1,13 @@
 import { Module } from "@nestjs/common";
 import { CommonModule } from "../common/common.module";
+import { EventDuplicationService } from "./event-duplication.service";
 import { EventsController } from "./events.controller";
 import { EventsService } from "./events.service";
 
 @Module({
   imports: [CommonModule],
   controllers: [EventsController],
-  providers: [EventsService],
-  exports: [EventsService],
+  providers: [EventsService, EventDuplicationService],
+  exports: [EventsService, EventDuplicationService],
 })
 export class EventsModule {}
