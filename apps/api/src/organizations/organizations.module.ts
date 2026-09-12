b87db71c61c8da89a4aa40e7ea1640babf3f8924
Amanda Leandro Soares Do Carmo <41752743+amandaleandro@users.noreply.github.com>
2026-09-12 09:42:20 -0300

    feat(n4): register promoter performance in organizations module

--- apps/api/src/organizations/organizations.module.ts
+++ apps/api/src/organizations/organizations.module.ts
@@ -1,12 +1,14 @@
 import { Module } from "@nestjs/common";
 import { CommonModule } from "../common/common.module";
+import { PromoterPerformanceController } from "../promoter-performance/promoter-performance.controller";
+import { PromoterPerformanceService } from "../promoter-performance/promoter-performance.service";
 import { OrganizationsController } from "./organizations.controller";
 import { OrganizationsService } from "./organizations.service";
 
 @Module({
   imports: [CommonModule],
-  controllers: [OrganizationsController],
-  providers: [OrganizationsService],
+  controllers: [OrganizationsController, PromoterPerformanceController],
+  providers: [OrganizationsService, PromoterPerformanceService],
   exports: [OrganizationsService],
 })
 export class OrganizationsModule {}
