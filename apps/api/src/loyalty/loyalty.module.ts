ee27ac983171354ebafca67d4836503b63fb9753
Amanda Leandro Soares Do Carmo <41752743+amandaleandro@users.noreply.github.com>
2026-09-14 12:50:48 -0300

    feat(n9.1): register reward and buyer loyalty controllers

--- apps/api/src/loyalty/loyalty.module.ts
+++ apps/api/src/loyalty/loyalty.module.ts
@@ -1,11 +1,14 @@
 import { Module } from "@nestjs/common";
 import { CommonModule } from "../common/common.module";
 import { LoyaltyController } from "./loyalty.controller";
+import { LoyaltyRewardsController } from "./loyalty-rewards.controller";
+import { LoyaltyRewardsService } from "./loyalty-rewards.service";
 import { LoyaltyService } from "./loyalty.service";
+import { MeLoyaltyController } from "./me-loyalty.controller";
 
 @Module({
   imports: [CommonModule],
-  controllers: [LoyaltyController],
-  providers: [LoyaltyService],
+  controllers: [LoyaltyController, LoyaltyRewardsController, MeLoyaltyController],
+  providers: [LoyaltyService, LoyaltyRewardsService],
 })
 export class LoyaltyModule {}
