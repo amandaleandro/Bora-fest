054f0f36f4e86fceb881ee6ea601e62e97141c5e
Amanda Leandro Soares Do Carmo <41752743+amandaleandro@users.noreply.github.com>
2026-09-23 14:08:23 -0300

    feat(face): registra serviço e controller facial

--- apps/api/src/checkins/checkins.module.ts
+++ apps/api/src/checkins/checkins.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from "../common/common.module";
 import { ValidatorModule } from "../validator/validator.module";
 import { CheckinsController } from "./checkins.controller";
 import { CheckinsService } from "./checkins.service";
+import { FaceCheckinController } from "./face-checkin.controller";
+import { FaceCheckinService } from "./face-checkin.service";
 
 @Module({
   imports: [CommonModule, ValidatorModule],
-  controllers: [CheckinsController],
-  providers: [CheckinsService],
+  controllers: [CheckinsController, FaceCheckinController],
+  providers: [CheckinsService, FaceCheckinService],
 })
 export class CheckinsModule {}
